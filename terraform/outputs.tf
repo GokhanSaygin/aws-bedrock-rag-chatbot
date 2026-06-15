bb74aa3462b8c6a2d82e116bdbfa073cdd5771cd
Gokhan Saygin <agokhan.saygin@gmail.com>
2026-06-14 21:44:18 -0400

    Update Terraform outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -18,46 +18,6 @@ output "lambda_function_arn" {
 }
 
 
-resource "aws_apigatewayv2_api" "chatbot_api" {
-  name          = "${var.project_name}-api"
-  protocol_type = "HTTP"
-}
-
-resource "aws_apigatewayv2_integration" "lambda_integration" {
-  api_id                 = aws_apigatewayv2_api.chatbot_api.id
-  integration_type       = "AWS_PROXY"
-  integration_uri        = aws_lambda_function.chatbot.invoke_arn
-  integration_method     = "POST"
-  payload_format_version = "2.0"
-}
-
-resource "aws_apigatewayv2_route" "post_chat" {
-  api_id    = aws_apigatewayv2_api.chatbot_api.id
-  route_key = "POST /chat"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
-}
-
-resource "aws_apigatewayv2_route" "options_chat" {
-  api_id    = aws_apigatewayv2_api.chatbot_api.id
-  route_key = "OPTIONS /chat"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
-}
-
-resource "aws_apigatewayv2_stage" "default" {
-  api_id      = aws_apigatewayv2_api.chatbot_api.id
-  name        = "$default"
-  auto_deploy = true
-}
-
-resource "aws_lambda_permission" "allow_api_gateway" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.chatbot.function_name
-  principal     = "apigateway.amazonaws.com"
-
-  source_arn = "${aws_apigatewayv2_api.chatbot_api.execution_arn}/*/*"
-}
-
 output "api_endpoint" {
   description = "API Gateway endpoint URL"
   value       = aws_apigatewayv2_api.chatbot_api.api_endpoint
